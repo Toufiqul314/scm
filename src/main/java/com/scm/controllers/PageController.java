0f9b0972ba482ad09b4d4d07841e0117d1b9d4be
Toufiqul314 <islamtusher4@gmail.com>
2025-07-13 09:52:44 +0600

    Add PageController and home.html template for home page

---
+++ src/main/java/com/scm/controllers/PageController.java
@@ -0,0 +1,15 @@
+package com.scm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("name", "Spring Boot SCM Application");
+        return "home";
+    }
+}
