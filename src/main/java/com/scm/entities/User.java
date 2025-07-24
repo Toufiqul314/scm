e55b138cc0cae155c3f9211a26c4f759588ca4cc
Toufiqul314 <islamtusher4@gmail.com>
2025-07-24 16:01:28 +0600

    Add Providers enum and implement user entity with provider support

---
+++ src/main/java/com/scm/entities/User.java
@@ -0,0 +1,43 @@
+package com.scm.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "user")
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    private String userId;
+    @Column(name = "user_name", nullable = false)
+    private String name;
+    @Column(unique = true, nullable = false)
+    private String email;
+    private String password;
+    @Column(length = 500)
+    private String about;
+    @Column(length = 500)
+    private String profilePic;
+    private String phoneNumber;
+
+    // information
+    private boolean enabled = false;
+    private boolean emailVerified = false;
+    private boolean phoneVerified = false;
+
+    // SELF,GOOGLE,FACEBOOK,LINKEDIN,APPLE,GITHUB
+    private Providers provider = Providers.SELF;
+    private String providerUserId;
+}
