5c791faecd4b37ee6c10c2cd67dab1c0d51e2bea
Toufiqul314 <islamtusher4@gmail.com>
2025-07-25 11:14:59 +0600

    Add Contact and SocialLink entities with relationships to User

--- src/main/java/com/scm/entities/User.java
+++ src/main/java/com/scm/entities/User.java
@@ -1,8 +1,14 @@
 package com.scm.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -40,4 +46,11 @@ public class User {
     // SELF,GOOGLE,FACEBOOK,LINKEDIN,APPLE,GITHUB
     private Providers provider = Providers.SELF;
     private String providerUserId;
+
+    // add more fields if needed
+
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,fetch = FetchType.LAZY,orphanRemoval = true)
+   private List<Contact>contacts=new ArrayList<>();
+
+   
 }
