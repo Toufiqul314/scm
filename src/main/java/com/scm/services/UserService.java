20f90a094960bfe449f6c3a78b6b26b44ec8cf09
Toufiqul314 <islamtusher4@gmail.com>
2025-08-11 07:16:51 +0600

    Refactor UserService methods to return Optional<User> for better null handling

--- src/main/java/com/scm/services/UserService.java
+++ src/main/java/com/scm/services/UserService.java
@@ -1,6 +1,7 @@
 package com.scm.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.scm.entities.User;
 
@@ -8,9 +9,9 @@ public interface UserService {
 
     User saveUser(User user);
 
-    User getUserById(String id);
+    Optional<User> getUserById(String id);
 
-    User updateUser(User user);
+    Optional<User> updateUser(User user);
 
     void deleteUser(String id);
 
@@ -19,4 +20,6 @@ public interface UserService {
     boolean isUserExistByEmail(String email);
 
     List<User> getAllUsers();
+
+    // add more methods here relatesd user service[logic]
 }
