e8af9d00256e0c48a30c21ca8131dbad5216c4e3
Toufiqul314 <islamtusher4@gmail.com>
2025-08-12 08:26:28 +0600

    Implement message handling in user registration process and add session management for messages

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -10,7 +10,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.scm.entities.User;
 import com.scm.forms.UserForm;
+import com.scm.helpers.Message;
+import com.scm.helpers.MessageType;
 import com.scm.services.UserService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class PageController {
@@ -60,7 +64,7 @@ public class PageController {
 
     // processing register page
     @RequestMapping(value = "/do-register", method = RequestMethod.POST)
-    public String processRegister(@ModelAttribute UserForm userForm) {
+    public String processRegister(@ModelAttribute UserForm userForm, HttpSession session) {
         System.out.println("Processing Register Form");
         // fetch form data
         // UserForm
@@ -72,20 +76,34 @@ public class PageController {
         // show success message
 
         // userservice
-        // UserForm --> User
-        User user = User.builder()
-                .name(userForm.getName())
-                .email(userForm.getEmail())
-                .password(userForm.getPassword())
-                .about(userForm.getAbout())
-                .phoneNumber(userForm.getPhoneNumber())
-                .profilePic(
-                        "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg")
-                .build();
+        /*
+         * // UserForm --> User
+         * User user = User.builder()
+         * .name(userForm.getName())
+         * .email(userForm.getEmail())
+         * .password(userForm.getPassword())
+         * .about(userForm.getAbout())
+         * .phoneNumber(userForm.getPhoneNumber())
+         * .profilePic(
+         * "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg")
+         * .build();
+         */
+        User user = new User();
+        user.setName(userForm.getName());
+        user.setEmail(userForm.getEmail());
+        user.setPassword(userForm.getPassword());
+        user.setAbout(userForm.getAbout());
+        user.setPhoneNumber(userForm.getPhoneNumber());
+        user.setProfilePic(
+                "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg");
         User saveUser = userService.saveUser(user);
 
         System.out.println("user saved.");
         // message= "registered successfully";
+
+        // add the message
+        Message message = Message.builder().content("Registered Successfully !!").type(MessageType.green).build();
+        session.setAttribute("message", message);
 
         // redirect login page
         return "redirect:/register";
