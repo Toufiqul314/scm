3f774bf5b05191a8e26477e01d5c1f16c84aa050
Toufiqul314 <islamtusher4@gmail.com>
2025-08-21 08:32:23 +0600

    Enhance user registration form validation and error handling

--- src/main/java/com/scm/forms/UserForm.java
+++ src/main/java/com/scm/forms/UserForm.java
@@ -1,5 +1,7 @@
 package com.scm.forms;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,9 +17,16 @@ import lombok.ToString;
 @ToString
 public class UserForm {
 
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, message = "Username must be at least 3 characters long")
     private String name;
+    @NotBlank(message = "Email is required")
     private String email;
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
+    @NotBlank(message = "About is required")
     private String about;
+    @Size(min = 11,max = 11, message = "Invalid phone number")
     private String phoneNumber;
 }
