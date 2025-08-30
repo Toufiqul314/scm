260914b41cbd4cffe8fc4c2fe68c7f6d713f5022
Toufiqul314 <islamtusher4@gmail.com>
2025-08-30 07:16:35 +0600

    Add UserController and user dashboard/profile HTML templates

---
+++ src/main/java/com/scm/controllers/UserController.java
@@ -0,0 +1,31 @@
+package com.scm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    // user dashboard page
+    @RequestMapping(value = "/dashboard")
+    public String userDashboard() {
+        return "user/dashboard";
+    }
+
+    // user profile page
+    @RequestMapping(value = "/profile")
+    public String userProfile() {
+        return "user/profile";
+    }
+
+    // user and contacts page
+
+    // user view contact
+
+    // user edit contact
+
+    // user delete contact
+
+    // user serch contact
+}
